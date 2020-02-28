c2fb22ab8baec953391fe84da377a4f47f3c878a
Edward Stoner <edwardlstoner@gmail.com>
2020-02-27 17:38:51 -0700

    Add logic for displaying trips

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import './css/base.scss';
 import User from './User';
 import FilterData from './FilterData';
 import domUpdates from './domUpdates'
+import './images/book.png'
+var moment = require('moment');
 
 const logInBtn = $('.log-in')
 const body = $('body')
 
 logInBtn.on('click', clearHTML)
+console.log(moment().format())
 
 function clearHTML() {
   body.removeClass('log-in')
@@ -15,11 +18,17 @@ function clearHTML() {
   onStartup(randomUser)
   body.html(`
     <header><h2>Travel Tracker</h2><h3 id="user-name"></h3></header>
-    <aside><h3>Book a trip</h3></aside>
+    <aside>
+      <div class="spent-container"><span id="spent-this-year"></span>spent on trips this year</div>
+      <div class="book-trip">
+        <img class="book-icon" src="./images/book.png">
+        <h3>Book a trip</h3>
+      </div>
+    </aside>
     <main>
-      <div class="current"><h3>You have no current trips</h3></div>
-      <div class="pending-upcoming"><h3>Pending and upcoming trips</h3></div>
-      <div class="past"><h3>Past trips</h3></div>
+      <div class="current"></div>
+      <div class="upcoming-wrapper"><h3>Upcoming trips</h3><div class="pending-upcoming"></div></div>
+      <div class="past-wrapper"><h3>Past trips</h3><div class="past"></div></div>
     </main>
 
   `);
@@ -33,5 +42,11 @@ function onStartup(userID) {
     .then(user => {
       console.log(user)
       domUpdates.displayName(user.name)
+      console.log(user.getCostOfTripsThisYear())
+      domUpdates.displaySpentThisYear(Math.round(user.getCostOfTripsThisYear()))
+      console.log(user.getUpcomingTrips())
+      console.log(user.getPastTrips())
+      domUpdates.displayPendingUpcoming(user.getUpcomingTrips())
+      domUpdates.displayPast(user.getPastTrips())
     })
 }
