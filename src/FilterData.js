8c919a99a45bc9b115b8d106a55781a474e603f6
Edward Stoner <edwardlstoner@gmail.com>
2020-02-28 22:18:27 -0700

    Refactor data methods

--- src/FilterData.js
+++ src/FilterData.js
@@ -1,8 +1,11 @@
-import User from './User';
+import Agency from './Agency';
+import User from './User'
+import Trip from './Trip'
 var moment = require('moment');
 
 
 class FilterData {
+
   constructor(id) {
     this.allTrips = fetch("https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips")
       .then(data => data.json());
@@ -13,30 +16,52 @@ class FilterData {
   }
 
   getUser(userID) {
-    return Promise.all([this.user, this.getAllTripsForUser(userID)])
+    return Promise.all([this.user, this.allTrips, this.allDestinations])
       .then(promises => {
-        return new User(promises[0].id, promises[0].name, promises[0].travelerType, promises[1])
+        console.log(promises[0])
+        return new User(promises[0].id, promises[0].name, promises[0].travelerType, this.getAllTrips(promises[0].id, promises[1], promises[2], promises[0]))
+      })
+  }
+
+  getAgency() {
+    return Promise.all([this.user, this.allTrips, this.allDestinations])
+      .then(promises => {
+        console.log(promises[0])
+        return new Agency(promises[0].travelers.map(user => {
+          return new User(user.id, user.name, user.travelerType, this.getAllTrips(user.id, promises[1], promises[2], promises[0]))
+        }))
       })
   }
 
 
-  getAllTripsForUser(userID) {
-    return Promise.all([this.allTrips, this.allDestinations])
-      .then( data => {
-        const regex = /\//gi;
-        const filteredData = data[0].trips.filter(trip => trip.userID === userID)
-          return filteredData.map(trip => {
-            return {
-              id: trip.id,
-              destination: data[1].destinations.find(destination => destination.id === trip.destinationID),
-              travelers: trip.travelers,
-              date: moment(trip.date.replace(regex, '-')).format(),
-              duration: trip.duration,
-              status: trip.status
-            }
-         })
-      })
+
+
+
+  // getAllTripsForUser(userID) {
+  //   return Promise.all([this.allTrips, this.allDestinations])
+  //     .then( data => {
+  //       console.log(data[1])
+  //       let trips = data[0];
+  //       let destinations = data[1];
+  //       console.log(destinations)
+  //       const filteredData = trips.trips.filter(trip => trip.userID === userID)
+  //       return filteredData.map(trip => {
+  //         return new Trip(trip, destinations)
+  //      })
+  //     })
+  // }
+
+
+  getAllTrips(userID, trips, destinations, users) {
+    console.log(users)
+    const regex = /\//gi;
+    const filteredData = trips.trips.filter(trip => trip.userID === userID)
+    return filteredData.map(trip => {
+      return new Trip(trip, destinations, users)
+   })
   }
+
+
 
 }
 
