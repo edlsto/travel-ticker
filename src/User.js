8d0b67dfefbe394c984d5c47c63a17265d819087
Edward Stoner <edwardlstoner@gmail.com>
2020-02-29 22:24:36 -0700

    Add method for requesting trip

--- src/User.js
+++ src/User.js
@@ -1,6 +1,6 @@
 import FilterData from './FilterData';
 var moment = require('moment');
-
+import Trip from './Trip'
 
 class User {
   constructor(id, name, type, trips) {
@@ -10,7 +10,6 @@ class User {
   }
 
   getCostOfTripsThisYear() {
-    console.log(this)
     const tripsThisYear = this.trips.filter(trip => {
       return moment(trip.date).isAfter('2020-01-01')
     })
@@ -43,6 +42,30 @@ class User {
       return moment(trip.date).isAfter(moment()) && trip.status === 'pending'
     })
   }
+
+  requestTrip(tripID, destinationID, travelers, date, duration) {
+    console.log(tripID, destinationID, travelers, date, duration)
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "id": tripID,
+        "userId": this.id,
+        "destinationId": destinationID,
+        "travelers": travelers,
+        "date": date,
+        "duration": 8,
+        "status": "pending",
+        "suggestedActivities": []
+      })
+    }).then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error.message))
+  }
+
+
 
 }
 
