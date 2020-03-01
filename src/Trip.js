917a2eb26492921f98d284d2fbceac37f9f7e901
Edward Stoner <edwardlstoner@gmail.com>
2020-03-01 12:33:14 -0700

    Remove or for ID and Id

--- src/Trip.js
+++ src/Trip.js
@@ -4,7 +4,7 @@ class Trip {
   constructor(tripData, destinationsData, users) {
     this.userID = tripData.userID;
     this.id = tripData.id
-    this.destination = destinationsData.destinations.find(destination => destination.id === tripData.destinationID || destination.id === tripData.destinationId)
+    this.destination = destinationsData.destinations.find(destination => destination.id === tripData.destinationID)
     this.travelers = tripData.travelers;
     this.date = moment(tripData.date.replace(/\//gi, '-')).format();
     this.duration = tripData.duration;
