2c5ef81e895a4190bf0d0f39510e2bf2459ce9ce
Edward Stoner <edwardlstoner@gmail.com>
2020-02-29 22:15:47 -0700

    Remove console logs

--- src/Agency.js
+++ src/Agency.js
@@ -22,6 +22,28 @@ class Agency extends User {
     }, [])
   }
 
+  getPendingTrips(trips) {
+    return trips.filter(trip => {
+      return trip.status === "pending"
+    })
+  }
+
+  // deleteTrip(tripID) {
+  //   fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips', {
+  //     method: 'DELETE',
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({
+  //       "id": tripID
+  //     })
+  //   }).then(response => response.json())
+  //     .then(json => console.log(json))
+  //     .catch(error => console.log(error.message))
+  // }
+
+
+
 }
 
 
