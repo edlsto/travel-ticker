ab9dc6c78589bfa70f829638536cc27700fb30d8
Edward Stoner <edwardlstoner@gmail.com>
2020-03-01 22:08:53 -0700

    Add method for income generated

--- src/Agency.js
+++ src/Agency.js
@@ -7,6 +7,13 @@ class Agency extends User {
     this.users = users
   }
 
+  incomeGenerated(trips) {
+    return trips.reduce((totalFees, trip) => {
+      totalFees += trip.cost * .1
+      return totalFees;
+    }, 0)
+  }
+
   accessUserInfo(user) {
     this.id = user.id;
     this.name = user.name;
@@ -22,10 +29,41 @@ class Agency extends User {
     }, [])
   }
 
-  getPendingTrips(trips) {
+  getAllPendingTrips(trips) {
     return trips.filter(trip => {
       return trip.status === "pending"
     })
+  }
+
+  approveTrip(id) {
+    console.log('post' + id)
+    return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/updateTrip', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "id": id,
+        "status": "approved"
+      })
+    }).then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error.message))
+  }
+
+  denyTrip(id) {
+    console.log('delete' + id)
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips', {
+      method: 'DELETE',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "id": id,
+      })
+    }).then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error.message))
   }
 
   // deleteTrip(tripID) {
